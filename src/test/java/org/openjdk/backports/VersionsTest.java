fc1ac2ff3e2070afd442760478a23a041d664a93
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2020-01-14 19:57:18 +0100

    Cleanup versions

--- src/test/java/org/openjdk/backports/VersionsTest.java
+++ src/test/java/org/openjdk/backports/VersionsTest.java
@@ -2,7 +2,6 @@ package org.openjdk.backports;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openjdk.backports.jira.Parsers;
 import org.openjdk.backports.jira.Versions;
 
 public class VersionsTest {
@@ -87,36 +86,6 @@ public class VersionsTest {
         Assert.assertEquals(-1,  Versions.parseMinor("openjdk7u"));
         Assert.assertEquals(-1,  Versions.parseMinor("openjdk8u"));
         Assert.assertEquals(-1,  Versions.parseMinor("openjdk8u212"));
-    }
-
-    static String SAMPLE_COMMENT = "URL: http://hg.openjdk.java.net/jdk/jdk/rev/66f5241da404\n" +
-            "User: shade\n" +
-            "Date: 2019-04-15 16:22:25 +0000";
-
-    @Test
-    public void parseURL() {
-        Assert.assertEquals(
-                "http://hg.openjdk.java.net/jdk/jdk/rev/66f5241da404",
-                Parsers.parseURL(SAMPLE_COMMENT)
-        );
-    }
-
-    @Test
-    public void parseUser() {
-        Assert.assertEquals(
-                "shade",
-                Parsers.parseUser(SAMPLE_COMMENT)
-        );
-    }
-
-    @Test
-    public void parseDayAgo() {
-        Assert.assertTrue(Parsers.parseDaysAgo(SAMPLE_COMMENT) > 0);
-    }
-
-    @Test
-    public void parseSecondsAgo() {
-        Assert.assertTrue(Parsers.parseSecondsAgo(SAMPLE_COMMENT) > 0);
     }
 
 }
