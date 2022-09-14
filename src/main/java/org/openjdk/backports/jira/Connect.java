db81f2672d111234d459fe67896fc287704fe2fb
Wen Zhou <wenzhou@redhat.com>
2022-09-14 09:36:03 +0200

    chore: polish code

--- src/main/java/org/openjdk/backports/jira/Connect.java
+++ src/main/java/org/openjdk/backports/jira/Connect.java
@@ -28,9 +28,10 @@ import com.atlassian.event.api.EventPublisher;
 import com.atlassian.httpclient.apache.httpcomponents.DefaultHttpClientFactory;
 import com.atlassian.httpclient.api.HttpClient;
 import com.atlassian.httpclient.api.factory.HttpClientOptions;
+import com.atlassian.jira.rest.client.api.AuthenticationHandler;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
 import com.atlassian.jira.rest.client.auth.AnonymousAuthenticationHandler;
+import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClient;
 import com.atlassian.jira.rest.client.internal.async.AtlassianHttpClientDecorator;
 import com.atlassian.jira.rest.client.internal.async.DisposableHttpClient;
@@ -78,29 +79,18 @@ public class Connect {
 
         HttpClient client = factory.create(opts);
 
+        AuthenticationHandler auth = new AnonymousAuthenticationHandler();
         if (user != null && pass != null) {
-            DisposableHttpClient dispClient = new AtlassianHttpClientDecorator(
-                    client,
-                    new BasicHttpAuthenticationHandler(user, pass)) {
-                        @Override public void destroy() throws Exception { factory.dispose(client); }
-            };
-
-            return new Clients(
-                    new AsynchronousJiraRestClient(uri, dispClient),
-                    new RawRestClient(uri, dispClient)
-            );
-        } else { // change to use anoymous auth
-            DisposableHttpClient dispClient = new AtlassianHttpClientDecorator(
-                    client,
-                    new AnonymousAuthenticationHandler()) {
-                        @Override public void destroy() throws Exception { factory.dispose(client); }
-            };
-
-            return new Clients(
-                    new AsynchronousJiraRestClient(uri, dispClient),
-                    new RawRestClient(uri, dispClient)
-            );
+            auth = new BasicHttpAuthenticationHandler(user, pass);
         }
+        DisposableHttpClient dispClient = new AtlassianHttpClientDecorator(client, auth) {
+            @Override public void destroy() throws Exception { factory.dispose(client); }
+        };
+
+        return new Clients(
+                new AsynchronousJiraRestClient(uri, dispClient),
+                new RawRestClient(uri, dispClient)
+        );
     }
 
     private static class MyEventPublisher implements EventPublisher {
